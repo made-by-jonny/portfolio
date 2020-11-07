c9547e7d3c11fca590deb3c7267eaa579a536beb
jonathan hamilton <jonathanhamilton@jonathans-MacBook-Pro.local>
2020-11-07 15:00:06 +0000

    added static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,4 +4,11 @@ const Index: React.FC = () => {
   return <h1>hello</h1>;
 };
 
+export async function getStaticProps() {
+  return {
+    props: {},
+    revalidate: 1,
+  };
+}
+
 export default Index;
